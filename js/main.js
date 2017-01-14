e96514f4fa5209cba644a1565e765421dbf6a605
Sanket Mishra <sanket14mishra@gmail.com>
2017-01-15 02:49:15 +0530

    Added close button functionality

--- js/main.js
+++ js/main.js
@@ -6,35 +6,32 @@ $('document').ready(function() {
 		if(!isFull){
 			$(this).addClass("full z-index");
 			$(this).one("transitionend", function() {
-				$('div.event.full div.event-content-wrapper.hidden')
+				$('div.event-content-wrapper.hidden')
 					.addClass("visible");
-			});
+				});
+
 
 			isFull=true;
 		} 
 
-		else{
-			$('div.event.full div.event-content-wrapper')
+		$('div.close-button').click(function() {
+			$('div.event-content-wrapper.hidden')
 					.removeClass("visible");
-			$(this).removeClass("full");
-			
-			$(this).one("transitionend",function() {
-				$(this).removeClass("z-index");
+			$(dep).removeClass("full");
+			$(dep).one("transitionend", function() {
+				$(dep).removeClass("z-index");
+				isFull=false;
 			});
-			isFull=false;
-		}
+		});
+
 	});
+
+
 
 		$('div.event.full div.event-list li.item1').one("click",function() {
 				console.log("Clicked"+(++ck));
 				$('div.event.full div.event-description .item1')
 					.addClass("visible");
 			});
-
-	function closePanel(){
-		$(dep).removeClass("full z-index");
-		console.log("Rem");
-	}
-
 
 });
